a9ebd5e7d92a4a5506a0ceec8eb4dfeedadd12ad
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-25 01:55:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, render_template, send_file
+import rembg
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    if request.method == 'POST':
+        # get the uploaded file
+        file = request.files['file']
+        # remove the background of the image
+        output_file = rembg.remove_background(file)
+        # create a response object with the output image as the attachment
+        response = make_response(send_file(output_file, as_attachment=True, attachment_filename='output.jpg'))
+        return response
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
